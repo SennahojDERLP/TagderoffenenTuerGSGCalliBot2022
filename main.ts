c78e63b1b4cfdaa8aed512d140def8e757bb1063
SennahojDERLP <121190463+SennahojDERLP@users.noreply.github.com>
2022-12-21 21:29:09 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,14 @@ let Aktiv = 0
 let Auswahl = 0
 function Kreisfahren () {
     callibot.motor(KMotor.links, KDir.rückwärts, 100)
-    for (let index = 0; index < 100; index++) {
-        callibot.setRgbLed(KRgbLed.RV, KRgbColor.rot, 8)
+    for (let index = 0; index < 50; index++) {
+        callibot.setRgbLed(KRgbLed.RV, KRgbColor.gelb, 8)
         callibot.setLed(KMotor.rechts, KState.an)
         led.plot(0, 4)
         basic.pause(1000)
         led.unplot(0, 4)
         callibot.setLed(KMotor.rechts, KState.aus)
-        callibot.setRgbLed(KRgbLed.RV, KRgbColor.rot, 0)
+        callibot.setRgbLed(KRgbLed.RV, KRgbColor.gelb, 0)
     }
     callibot.motorStop(KMotor.links, KStop.Frei)
 }
@@ -22,7 +22,7 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     } else if (Aktiv == 3) {
         Vorwaertszurueck()
     } else if (Aktiv == 4) {
-    	
+        GL380()
     } else if (Aktiv == 5) {
     	
     } else if (Aktiv == 6) {
@@ -37,7 +37,7 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showString("Druecke B zur Auswahl und A zum Starten.")
 })
 function BlaulichtSirene () {
-    for (let index = 0; index < 100; index++) {
+    for (let index = 0; index < 15; index++) {
         callibot.setRgbLed(KRgbLed.All, KRgbColor.rot, 0)
         basic.showLeds(`
             # . . . #
@@ -71,11 +71,11 @@ function BlaulichtSirene () {
 function Vorwaertszurueck () {
     for (let index = 0; index < 10; index++) {
         callibot.motor(KMotor.beide, KDir.vorwärts, 100)
-        basic.pause(1000)
-        callibot.motorStop(KMotor.beide, KStop.Bremsen)
+        basic.pause(2000)
+        callibot.motorStop(KMotor.beide, KStop.Frei)
         callibot.motor(KMotor.beide, KDir.rückwärts, 100)
-        basic.pause(1000)
-        callibot.motorStop(KMotor.beide, KStop.Bremsen)
+        basic.pause(2000)
+        callibot.motorStop(KMotor.beide, KStop.Frei)
     }
 }
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
@@ -90,7 +90,7 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     } else if (Auswahl == 3) {
         basic.showString("Vorwaerts und Rueckwaertsfahren")
     } else if (Auswahl == 4) {
-    	
+        basic.showString("Grosser Gott wir Loben dich")
     } else if (Auswahl == 5) {
     	
     } else if (Auswahl == 6) {
@@ -101,3 +101,44 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
         basic.showString("Fehler")
     }
 })
+function GL380 () {
+    for (let index = 0; index < 2; index++) {
+        music.playTone(349, music.beat(BeatFraction.Double))
+        music.playTone(349, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(349, 3)
+        music.playTone(440, music.beat(BeatFraction.Double))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(466, music.beat(BeatFraction.Whole))
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        music.playTone(466, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Whole))
+    }
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.playTone(466, music.beat(BeatFraction.Whole))
+    music.playTone(466, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Double))
+    music.playTone(466, music.beat(BeatFraction.Whole))
+    music.playTone(523, 3)
+    music.playTone(587, music.beat(BeatFraction.Double))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(466, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(466, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(349, 3)
+}
